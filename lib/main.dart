a506efb332951ebcbb3c4e3a3a5ed407d0954465
flutterlabz <flutterlabz@gmail.com>
2022-01-20 10:31:15 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutterx/core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutterx/shared/util/theme/theme.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:universal_io/io.dart';
 
@@ -15,46 +15,7 @@ void main() async {
 
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      primaryColor: primaryColor,
-      scaffoldBackgroundColor: Colors.grey[100],
-      cardTheme: CardTheme(
-        elevation: 0.6,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-      ),
-      iconTheme: IconThemeData(
-        color: Colors.grey[800],
-      ),
-      appBarTheme: AppBarTheme(
-        elevation: 0.0,
-        backgroundColor: Colors.white,
-        titleTextStyle: TextStyle(
-          color: Colors.grey[800],
-        ),
-        actionsIconTheme: IconThemeData(
-          color: Colors.grey[800],
-        ),
-        iconTheme: IconThemeData(
-          color: Colors.grey[800],
-        ),
-      ),
-      textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-        headline1: TextStyle(
-          color: Colors.grey[800],
-        ),
-        headline2: TextStyle(
-          color: Colors.grey[800],
-        ),
-        bodyText1: TextStyle(
-          color: Colors.grey[800],
-        ),
-        bodyText2: TextStyle(
-          color: Colors.grey[800],
-        ),
-      ),
-    ),
+    theme: defaultTheme,
     home: MainNavigationView(),
   ));
 
