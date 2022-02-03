e2e8e17b86119942fd70dda269fddf64fee5a765
flutterlabz <flutterlabz@gmail.com>
2022-02-03 16:08:32 +0700

    .

--- lib/service/product_service/product_service.dart
+++ lib/service/product_service/product_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutterx/core.dart';
-import 'package:flutterx/shared/util/firebase/firecore.dart';
 
 class ProductService extends FireCore {
   @override
--- lib/shared/widget/fire_stream/fire_crud.dart
+++ lib/shared/widget/fire_stream/fire_crud.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterx/core.dart';
-import 'package:flutterx/shared/util/firebase/firecore.dart';
 
 class FireListItem {
   final String? title;
