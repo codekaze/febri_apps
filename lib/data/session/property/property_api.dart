cd1a491b557e0508f5adfde3e101c81e671cbd6c
flutterlabz <flutterlabz@gmail.com>
2022-02-04 16:00:42 +0700

    .

---
+++ lib/data/session/property/property_api.dart
@@ -0,0 +1,44 @@
+class PropertyApi {
+  static List products = [
+    {
+      "product_name": "Green House",
+      "photo_url":
+          "https://i.ibb.co/5jS6nZg/photo-1518780664697-55e3ad937233-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+      "price": 13500,
+      "room_count": 6,
+      "bath_room_count": 2,
+      "land_size": 2400,
+      "address": "2BW Street,NY,New York",
+    },
+    {
+      "product_name": "Red House",
+      "photo_url":
+          "https://i.ibb.co/tqkS0Pc/photo-1570129477492-45c003edd2be-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+      "price": 14500,
+      "room_count": 8,
+      "bath_room_count": 2,
+      "land_size": 3000,
+      "address": "3BW Street,NY,New York",
+    },
+    {
+      "product_name": "Blue House",
+      "photo_url":
+          "https://i.ibb.co/mDxKBFx/photo-1564013799919-ab600027ffc6-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+      "price": 17500,
+      "room_count": 12,
+      "bath_room_count": 2,
+      "land_size": 4000,
+      "address": "4BW Street,NY,New York",
+    },
+    {
+      "product_name": "Yellow House",
+      "photo_url":
+          "https://i.ibb.co/0cvnqQW/photo-1568605114967-8130f3a36994-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+      "price": 18500,
+      "room_count": 10,
+      "bath_room_count": 2,
+      "land_size": 5000,
+      "address": "5BW Street,NY,New York",
+    },
+  ];
+}
