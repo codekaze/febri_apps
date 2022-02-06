b173984ce0e16bf04b3d908828bf3f3b1391223e
flutterlabz <flutterlabz@gmail.com>
2022-02-06 08:48:07 +0700

    .

--- lib/module/app/job_listing/jo_dashboard/view/jo_dashboard_view.dart
+++ lib/module/app/job_listing/jo_dashboard/view/jo_dashboard_view.dart
@@ -2,9 +2,6 @@ import 'package:fireverse/fireverse.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterx/core.dart';
 
-
-
-
 class JoDashboardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -21,42 +18,7 @@ class JoDashboardView extends StatelessWidget {
             title: Text("JoDashboard"),
           ),
           body: Column(
-            children: [
-              ExButton(
-                label: "Add",
-                onPressed: () async {
-                  ref.add({
-                    "product_name": "Hello",
-                    "price": 250,
-                  });
-                },
-              ),
-              Expanded(
-                child: StreamBuilder(
-                  stream: ref.stream,
-                  builder: (context, stream) {
-                    if (stream.data == null) return Container();
-                    List items = (stream.data as List);
-                    if (items.length == 0) return Container();
-
-                    return ListView.builder(
-                      itemCount: items.length,
-                      shrinkWrap: shrinkWrap,
-                      physics: shrinkWrap == false
-                          ? null
-                          : NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        var item = items[index];
-                        var docId = items[index].id;
-                        item["id"] = docId;
-
-                        return Container();
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
+            children: [],
           ),
         );
       },
